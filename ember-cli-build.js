16a12ec3f2c8d2ff0ef72795dedb4502d2fdf00b
Ilya Radchenko <ilya@burstcreations.com>
2016-04-07 16:07:00 -0400

    Include polyfill

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,9 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
+    babel: {
+      includePolyfill: true
+    }
     // Add options here
   });
 
