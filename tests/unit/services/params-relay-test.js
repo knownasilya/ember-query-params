8a2c4c46f0103bb2151d4d5226323126d89c67c7
Ilya Radchenko <ilya@burstcreations.com>
2016-04-07 16:32:42 -0400

    Add more tests

--- tests/unit/services/params-relay-test.js
+++ tests/unit/services/params-relay-test.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import { moduleFor, test } from 'ember-qunit';
 
 moduleFor('service:params-relay', 'Unit | Service | params relay', {
@@ -15,4 +16,34 @@ test('setting a param', function(assert) {
   service.setParam('name', 'john');
 
   assert.equal(service.getParam('name'), 'john');
+});
+
+test('subscribe fired on set', function(assert) {
+  let service = this.subject();
+
+  service.subscribe('test', (key, val) => {
+    assert.equal(key, 'test');
+    assert.equal(val, 'blah');
+  });
+
+  assert.notOk(service.getParam('test'));
+  service.setParam('test', 'blah');
+});
+
+test('autoSubscribe changes values', function(assert) {
+  let service = this.subject();
+  let target = Ember.Object.create({
+    queryParams: [
+      'hello',
+      { myName: 'name' }
+    ]
+  });
+
+  service.autoSubscribe(target);
+
+  service.setParam('hello', 'bob');
+  service.setParam('myName', 'john');
+
+  assert.equal(target.get('hello'), 'bob');
+  assert.equal(target.get('myName'), 'john');
 });
