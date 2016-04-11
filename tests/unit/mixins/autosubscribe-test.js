67b41f2593bbd8d44bbdf137952ceafbd68b738e
Ilya Radchenko <ilya@burstcreations.com>
2016-04-11 16:44:33 -0400

    Update autosubscribe test

--- tests/unit/mixins/autosubscribe-test.js
+++ tests/unit/mixins/autosubscribe-test.js
@@ -2,8 +2,9 @@ import Ember from 'ember';
 import { test, moduleFor } from 'ember-qunit';
 import emberVersionGTE from 'ember-test-helpers/has-ember-version';
 
-moduleFor('controller:test', {
-  needs: ['service:params-relay']
+moduleFor('route:test', {
+  //needs: ['service:params-relay', 'controller:test'],
+  integration: true
 });
 
 test('it works', function(assert) {
@@ -13,8 +14,8 @@ test('it works', function(assert) {
   relay.setParam('hello', 'bob');
   relay.setParam('myName', 'john');
 
-  assert.equal(subject.get('hello'), 'bob');
-  assert.equal(subject.get('myName'), 'john');
+  assert.equal(subject.controller.get('hello'), 'bob');
+  assert.equal(subject.controller.get('myName'), 'john');
 });
 
 test('auto unsubscribe', function(assert) {
@@ -29,8 +30,8 @@ test('auto unsubscribe', function(assert) {
   relay.setParam('hello', 'bob');
   relay.setParam('myName', 'john');
 
-  assert.equal(subject.get('hello'), 'bob');
-  assert.equal(subject.get('myName'), 'john');
+  assert.equal(subject.controller.get('hello'), 'bob');
+  assert.equal(subject.controller.get('myName'), 'john');
   assert.equal(local, 'bob');
 
   Ember.run(() => {
@@ -42,12 +43,12 @@ test('auto unsubscribe', function(assert) {
 
   // values stayed the same, since it's destroyed/ing
   if (emberVersionGTE(2,0)) {
-    assert.equal(subject.get('hello'), 'bob');
-    assert.equal(subject.get('myName'), 'john');
+    assert.equal(subject.controller.get('hello'), 'bob');
+    assert.equal(subject.controller.get('myName'), 'john');
   } else {
     // 1.13 returns `undefined` if controller destroyed
-    assert.equal(subject.get('hello'), undefined);
-    assert.equal(subject.get('myName'), undefined);
+    assert.equal(subject.controller.get('hello'), undefined);
+    assert.equal(subject.controller.get('myName'), undefined);
   }
   // other functions still present
   assert.equal(local, 'bob2');
